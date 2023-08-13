4a2d5ea9b05d9858075dd8eada56ef1e1cdbe52f
rani <raniestrich5030@gmail.com>
2023-08-13 11:40:53 +0300

    Added Feedforward

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
 
@@ -15,6 +16,10 @@ public class Shoot extends CommandBase {
     private static final Supplier<Double> kD = namespace.addConstantDouble("kD", 0);
     private final PIDController pidController;
 
+    private static final  Supplier<Double> kS = namespace.addConstantDouble("kS", 0);
+    private static final Supplier<Double> kV = namespace.addConstantDouble("kV", 0);
+    private final SimpleMotorFeedforward feedForward;
+
     private Shooter shooter;
     private static Supplier<Double> rotationsPerSecond = namespace.addConstantDouble("rotations per second", 0);
 
@@ -22,6 +27,7 @@ public class Shoot extends CommandBase {
         this.shooter = shooter;
         addRequirements(shooter);
         pidController = new PIDController(kP.get(), kI.get(), kD.get());
+        feedForward = new SimpleMotorFeedforward(kS.get(), kV.get());
     }
 
     public static void periodic() {
@@ -31,8 +37,8 @@ public class Shoot extends CommandBase {
     @Override
     public void execute() {
         pidController.setPID(kP.get(), kI.get(), kD.get());
-        shooter.setSpeed(shooter.getMotor().get() + (pidController.calculate(shooter.getEncoder().getVelocity(),
-                rotationsPerSecond.get())) * 0.02);
+        shooter.setSpeed((pidController.calculate(shooter.getEncoder().getVelocity(),
+                rotationsPerSecond.get())) * 0.02 + feedForward.calculate(rotationsPerSecond.get()));
     }
 
     @Override
