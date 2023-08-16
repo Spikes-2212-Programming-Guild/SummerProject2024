672aca728e54c49752e4cbb91b3d0c06ca9fad3f
rani <raniestrich5030@gmail.com>
2023-08-16 14:10:50 +0300

    deleted unnecessary imports from OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,12 +1,5 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.spikes2212.util.PlaystationControllerWrapper;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.Shoot;
-import frc.robot.subsystems.Shooter;
-
 public class OI {
 
 }
